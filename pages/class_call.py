d1f44b6df77b7d8763456ec47d6f17770e8c7baa
geometryweb <geometryweb007@gmail.com>
2025-02-24 23:37:47 +0900

    Add files via upload

--- pages/class_call.py
+++ pages/class_call.py
@@ -5,12 +5,15 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import streamlit as st
 from school import student
 
-sooni = student.Student(80,70,60,80,70)
-
+sooni = student.Student(80, 70, 60, 80, 70)
+malgun = student.Student(70, 80, 50, 70, 30)
 
 st.write("class test")
 st.write(f"총점: {sooni.student_sum()}/평균: {sooni.student_average():.1f}")
 # print(sooni.student_sum(), sooni.student_average())
-
+# print(sooni.__eq__(malgun))
+st.write("순이와 순이의 성적은 같다" ,sooni.__eq__(sooni))
+st.write("순이와 맑은이의 성적은 같다" ,sooni.__eq__(malgun))
+st.write("순이와 맑은이의 성적은 다르다",sooni.__ne__(malgun))
 
 
