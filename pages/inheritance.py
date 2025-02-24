ce40882b3f9b414504bad787941aa0f66ce6eff5
geometryweb <geometryweb007@gmail.com>
2025-02-25 02:45:54 +0900

    [class2]

--- pages/inheritance.py
+++ pages/inheritance.py
@@ -16,6 +16,16 @@ class Circle(Shape):
         print(f"원의 반지름은: {self.__radius}")
     def area(self): 
         return self.__PI * self.__radius**2
-    
+class Square(Shape):
+    def __init__(self, 가로, 세로):
+        self.__width = 가로
+        self.__height = 세로
+    def sub_display(self):
+        print(f"사각형의 가로: {self.__width} 사각형의 세로: {self.__height}")
+    def area(self):
+        return self.__width * self.__height
+        
 circle = Circle(10)
 circle.main_display()
+square = Square(10, 10)
+square.main_display()
