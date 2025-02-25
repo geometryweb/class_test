80fd79f5e650e32ef552bb06ce8e6f770f8db4f5
geometryweb <geometryweb007@gmail.com>
2025-02-25 20:25:29 +0900

    [class 최적화]

--- pages/inheritance.py
+++ pages/inheritance.py
@@ -3,32 +3,37 @@ import streamlit as st
 class Shape:
     def __init__(self):
         pass
+
     def main_display(self):
         st.write("_"*10)
-        st.write(f"{self.sub_display()}")
-        st.write(f"{self.__class__} 넓이: {self.area()}")
-        
+        st.write(self.sub_display())
+        st.write(f"{self.__class__.__name__} 넓이: {self.area()}")
+
 class Circle(Shape):
-    def __init__(self, 반지름):
-        self.__PI = 3.14
-        self.__radius = 반지름
+    PI = 3.14
+
+    def __init__(self, radius):
+        self._radius = radius
+
     def sub_display(self):
-        re = "원의 반지름은:"
-        return f"{re} {self.__radius}"
-    def area(self): 
-        return self.__PI * self.__radius**2
-class Square(Shape):
-    def __init__(self, 가로, 세로):
-        self.__width = 가로
-        self.__height = 세로
-    def sub_display(self):
-        return f"사각형 가로 길이: {self.__width} /사각형 세로 길이: {self.__height}"
+        return f"원의 반지름은: {self._radius}"
+
     def area(self):
-        return self.__width * self.__height
-    
+        return self.PI * self._radius**2
+
+class Square(Shape):
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    def sub_display(self):
+        return f"사각형 가로 길이: {self._width} / 사각형 세로 길이: {self._height}"
+
+    def area(self):
+        return self._width * self._height
+
 st.subheader("클래스")        
 circle = Circle(10)
 circle.main_display()
 square = Square(10, 10)
 square.main_display()
-
